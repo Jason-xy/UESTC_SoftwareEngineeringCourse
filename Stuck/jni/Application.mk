4557043bdbdd9fc12dbd3e0ab4e6c6c1210395da
jason-xy <1462357003@qq.com>
2019-11-25 20:40:13 +0800

    删除文件夹

--- Stuck/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-NDK_TOOLCHAIN_VERSION := 4.9
-APP_STL := gnustl_static
-APP_PIE := $(APP_PIE_REQUIRED)
-ifeq ($(HOST_ARCH),x86)
-    APP_ABI := x86
-else
-    APP_ABI := armeabi-v7a
-endif
--- Stuck/obj/local/armeabi-v7a/objs/hello/hello.o.d
+++
@@ -1,1 +0,0 @@
-obj/local/armeabi-v7a/objs/hello/hello.o: jni/hello.cpp
