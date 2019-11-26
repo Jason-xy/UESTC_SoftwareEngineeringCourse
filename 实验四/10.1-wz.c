8c65cfa03d0e1448efdc150f324187f5378f2455
virtuoso-afk <56375221+virtuoso-afk@users.noreply.github.com>
2019-11-26 12:54:47 +0800

    Create 10.1-wz.c
    这道题的judge其实有问题，有可能写错了交上去的对的。

---
+++ 实验四/10.1-wz.c
@@ -0,0 +1,80 @@
+#include <stdbool.h> /* C99 only */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STACK_SIZE 100
+
+/* external variables */
+char contents[STACK_SIZE];
+int top = 0;
+
+void stack_overflow(void)
+{
+    printf("Stack overflow\n");
+    exit(EXIT_FAILURE);
+}
+
+void stack_underflow(void)
+{
+    printf("Stack underflow\n");
+    exit(EXIT_FAILURE);
+}
+
+void make_empty(void)
+{
+    top = 0;
+}
+
+bool is_empty(void)
+{
+    return top == 0;
+}
+
+bool is_full(void)
+{
+    return top == STACK_SIZE;
+}
+
+void push(char ch)
+{
+    if (is_full())
+        stack_overflow();
+    else
+        contents[top++] = ch;
+}
+
+char pop(void)
+{
+    if (is_empty())
+        stack_underflow();
+    else
+        return contents[--top];
+
+    return '\0'; /* prevents compiler warning due to stack_underflow() call */
+}
+
+int main(void)
+{
+    char ch;
+    while (1) 
+    {
+        ch = getchar();
+        if (ch == 40 || ch == 123) 
+        {
+            push(ch);
+            continue;
+        } else if (contents[top - 1] == ch - 1 || contents[top - 1] == ch - 2)
+        {
+            pop();
+            continue;
+        }
+        else if (ch == '}' || ch == ')') push(ch);
+        else break;
+    }
+    if (top == 0)
+        printf("Parentheses/braces are nested properly");
+    else
+        printf("Parentheses/braces are NOT nested properly");
+        system("pause");
+    return 0;
+}
